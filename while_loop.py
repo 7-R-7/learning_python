468b84a3c376988396b57c52db81236c141ccb93
7-R-7 <ahmedreguieg19@gmail.com>
2025-07-12 07:28:05 +0200

    update file structure

--- while_loop.py
+++
@@ -1,8 +0,0 @@
-students = ["Mohamed", "Ahmed", "Mustapha", "Omar", "Ali"]
-
-a = 0
-
-while a < len(students):
-    print (f"#{str(a+1).zfill(3)} {students[a]}") # I added a+1 so the counting start with 1 not 0
-    a +=1
-print("*End*")
