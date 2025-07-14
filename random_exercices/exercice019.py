b464cfccefe378e247510f1dc97364e093219f9c
7-R-7 <ahmedreguieg19@gmail.com>
2025-07-14 06:53:04 +0200

    Simple code that removes a char from long string

--- random_exercices/exercice019.py
+++ random_exercices/exercice019.py
@@ -2,3 +2,12 @@ def rm_char(word, c):
     return word.replace(c, "")
 
 print(rm_char("CSx Sevenx", "x"))
+
+def rm_char2(word, c):
+  word_without_char = []
+  for char in word:
+    if char != c.lower() and char != c.upper():
+      word_without_char.append(char)
+  return "".join(word_without_char)
+
+print(rm_char2("CSx SevXenx", "x"))
