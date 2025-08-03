ec377b5fc1f8618c65a32ad6d91ddf13dae96265
7-R-7 <ahmedreguieg19@gmail.com>
2025-08-03 20:53:31 +0100

    tkinter label example

---
+++ tkinter/label.py
@@ -0,0 +1,26 @@
+# label = an area widget that hold text and/or an image within a window
+from tkinter import *
+
+window = Tk()
+
+logo = PhotoImage(file="logo.png")
+
+label = Label(window, 
+              text="Hello from CSeven", 
+              font=('Arial',40,'bold'), 
+              fg="#f8833f",           # fg -> foreground 
+              bg='black',               # bg -> background
+              relief=RAISED,            # relief -> border style
+              bd = 10,                  # bd -> border
+              padx=20,                  # padx -> padding x
+              pady=20,                  # pady -> padding y
+              image=logo,
+              compound= 'bottom'         # position of image
+            )            
+label.pack()
+
+#label.place(x=100, y=100) # position of label same as pack
+
+
+
+window.mainloop()
