313d22e8edb602803ac5c6a10b17c87b99fab09d
7-R-7 <ahmedreguieg19@gmail.com>
2025-08-03 20:46:27 +0100

    tkinter basic GUI

---
+++ tkinter/basic_gui.py
@@ -0,0 +1,15 @@
+# widgets = GUI elements : buttons, textboxes, labels, images
+# windows = serves as a container to hold or contain these widgets
+
+from tkinter import *
+
+window = Tk() # instantiate an instance of a window
+window.geometry("420x420")
+window.title("Basic GUI")
+
+logo = PhotoImage(file='logo.png')
+window.iconphoto(True,logo)
+window.config(background="#9CF1DF")
+
+
+window.mainloop() # place window on computer screen , listen for events
