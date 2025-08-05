f90a274eb0064b24b3e5b3a1bc7a28f9d97df543
7-R-7 <ahmedreguieg19@gmail.com>
2025-08-05 09:53:35 +0100

    tkinter basic GUI

--- tkinter/basic_gui.py
+++ tkinter/basic_gui.py
@@ -7,7 +7,7 @@ window = Tk() # instantiate an instance of a window
 window.geometry("420x420")
 window.title("Basic GUI")
 
-logo = PhotoImage(file='logo.png')
+logo = PhotoImage(file='icons/logo.png')
 window.iconphoto(True,logo)
 window.config(background="#9CF1DF")
 
